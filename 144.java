4deced22ed333ca618a85c85d21fdd40102e83b3
Pargunan123 <saravananhcq@gmail.com>
2026-07-11 12:41:28 +0530

    144.java

---
+++ 144.java
@@ -0,0 +1,23 @@
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     List<Integer>ans=new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+         Traversal(root);
+        return ans;
+    }
+     void Traversal(TreeNode root){
+        if(root==null) return;
+        ans.add(root.val);
+        Traversal(root.left);
+        Traversal(root.right);
+}
+}
